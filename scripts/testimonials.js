fda12a47d455244304933d168e8b11361ed9868d
zezhux <nomil3246@gmail.com>
2025-11-13 16:12:12 +0000

    weygdwieufh

---
+++ scripts/testimonials.js
@@ -0,0 +1,34 @@
+// FOR LATER, IGNORE FOR NOW
+const testimonialList = [
+    {
+        "rating": 4,
+        "name": "Bobbi",
+        "review": "A true gem of a friend!"
+    },
+    {
+        "rating": 4,
+        "name": "Bob",
+        "review": "This Rocks!"
+    },
+    {
+        "rating": 2,
+        "name": "Goob",
+        "review": "Very lazy, doesn't move for days."
+    },
+    {
+        "rating": 5,
+        "name": "Bobbert",
+        "review": "Mine's not just a rock. it's family!"
+    },
+    {
+        "rating": 5,
+        "name": "Bobsi",
+        "review": "My depression? Cured. Will to live? Restored!"
+    },
+    {
+        "rating": 5,
+        "name": "Bobberta",
+        "review": "Walking my pet rock every day has been a great way to get some exercise!"
+    }
+]
+
