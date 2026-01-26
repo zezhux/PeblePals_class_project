edf8d1048cd7462f59e47497556bde190c40efef
zezhux <nomil3246@gmail.com>
2026-01-26 12:03:31 +0000

    made add testimonial function

--- scripts/testimonials.js
+++ scripts/testimonials.js
@@ -35,11 +35,27 @@ const testimonialList = [
 let testimonialsBox = document.getElementById("testimonials-box");
 
 for (let review of testimonialList) {
+    drawNewTestimonial(review.name, review.review, review.rating)
 
+}
 
+let tForm = document.getElementById("t-form");
+function addTestimonial(event){
+    event.preventDefault();
+
+    let name = document.getElementById("t-name").value;
+    let message = document.getElementById("t-message").value;
+    let rating = document.querySelector('input[name = rating]:checked').value;
+
+    drawNewTestimonial(name, message, rating)
+}
+
+tForm.addEventListener("submit", addTestimonial)
+
+function drawNewTestimonial(name, message, rating){
     let stars =""
     for (let i=0; i < 5; i++){
-        if  (i < review.rating){
+        if  (i < rating){
             stars += `<img class="star-img" src="/assets/star_yellow.svg">`
         }
         else{
@@ -47,23 +63,23 @@ for (let review of testimonialList) {
         }
     }
 
-
-
+    
     let testimonialToAdd = `
         <div class="testimonial">
                     <div class="t-rating-row">
                         <div class="t-stars">
                             ${stars}
                         </div>
-                        <p class="t-name">~${review.name}</p>
+                        <p class="t-name">~${name}</p>
                     </div>
                     <p class="t-message">
-                        ${review.review}
+                        ${message}
                     </p>
                 
                 </div>
         `
 
     testimonialsBox.innerHTML += testimonialToAdd;
+
 
 }
